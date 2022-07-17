e7e6423d186042007ff7294f0f4e687280085f0a
Miguel <miguelandradebarreto2@gmail.com>
2022-07-17 11:28:27 -0300

    :sparkles: add development and production env

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,11 +46,7 @@ class MyDocument extends Document {
 
           <link rel='preconnect' href='https://fonts.googleapis.com' />
 
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin=''
-          />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
 
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
